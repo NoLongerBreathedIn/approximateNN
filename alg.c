8de86341c3db128a09e2f9280679709c66db17ec
Eyal Minsky-Fenick <eyjmfc@gmail.com>
2018-08-30 09:55:40 -0400

    Fixed bug.

--- alg.c
+++ alg.c
@@ -317,7 +317,7 @@ size_t *TWO_GONLY(det_results, cl_context c, cl_command_queue q,
 			 pointers, graph, ipts), ycnt, k, k);
     relMem(pointers);
     pointers = ipts;
-    BUFTYPE(ftype) dpts = MK_BUF_RW_NA(c, ftype, k * (k + 1) * ycnt);
+    BUFTYPE(ftype) dpts = MK_BUF_RW_RO(c, ftype, k * (k + 1) * ycnt);
     enqueueCopy2D(q, ftype, len, k * (k + 1), 0, dists, dpts, ycnt, k);
     relMem(dists);
     dists = dpts;
