314ff629789859df5b379c4be80485ac1e22d68f
Eyal Minsky-Fenick <eyjmfc@gmail.com>
2018-09-07 12:10:41 -0400

    Futzed with external computation to use pipes for input instead of files. Files are still used for output, because I want to check exit status before reading.

--- ann_save.c
+++ ann_save.c
@@ -12,38 +12,73 @@ void free_save(save_t *save) {
   free(save->bases);
 }
 
-void write_save(const save_t *save, FILE *f) {
-  fwrite(&save->tries, sizeof(int), 1, f);
-  fwrite(&save->n, sizeof(size_t), 1, f);
-  fwrite(&save->k, sizeof(size_t), 1, f);
-  fwrite(&save->d_short, sizeof(size_t), 1, f);
-  fwrite(&save->d_long, sizeof(size_t), 1, f);
-  fwrite(save->par_maxes, sizeof(size_t), save->tries, f);
-  fwrite(save->graph, sizeof(size_t), save->n * save->k, f);
+#define checked_fwrite(p, t, c, f) \
+  if(fwrite(p, sizeof(t), c, f) != (size_t)(c)) return(1)
+#define checked_fread(p, t, c, f) \
+  if(fread(p, sizeof(t), c, f) != (size_t)(c)) return(1)
+
+char write_save(const save_t *save, FILE *f) {
+  checked_fwrite(&save->tries, int, 1, f);
+  checked_fwrite(&save->n, size_t, 1, f);
+  checked_fwrite(&save->k, size_t, 1, f);
+  checked_fwrite(&save->d_short, size_t, 1, f);
+  checked_fwrite(&save->d_long, size_t, 1, f);
+  checked_fwrite(save->par_maxes, size_t, save->tries, f);
+  checked_fwrite(save->graph, size_t, save->n * save->k, f);
   for(int i = 0; i < save->tries; i++)
-    fwrite(save->which_par[i], sizeof(size_t),
-	   save->par_maxes[i] << save->d_short, f);
-  fwrite(save->row_means, sizeof(ftype), save->d_long, f);
-  fwrite(save->bases, sizeof(ftype),
-	 save->tries * save->d_long * save->d_short, f);
+    checked_fwrite(save->which_par[i], size_t,
+		   save->par_maxes[i] << save->d_short, f);
+  checked_fwrite(save->row_means, ftype, save->d_long, f);
+  checked_fwrite(save->bases, ftype,
+		 save->tries * save->d_long * save->d_short, f);
+  return(0);
 }
 
-#define mallocread(p, t, c, f) \
-  fread(p = malloc(sizeof(t) * (c)), sizeof(t), c, f)
+#define checked_malloc(p, t, c) \
+  if(((p) = malloc(sizeof(t) * (c))) == NULL) return(1)
 
-void read_save(save_t *save, FILE *f) {
-  fread(&save->tries, sizeof(int), 1, f);
-  fread(&save->n, sizeof(size_t), 1, f);
-  fread(&save->k, sizeof(size_t), 1, f);
-  fread(&save->d_short, sizeof(size_t), 1, f);
-  fread(&save->d_long, sizeof(size_t), 1, f);
-  mallocread(save->par_maxes, size_t, save->tries, f);
-  mallocread(save->graph, size_t, save->n * save->k, f);
-  save->which_par = malloc(sizeof(size_t *) * save->tries);
-  for(int i = 0; i < save->tries; i++)
-    mallocread(save->which_par[i], size_t,
-	       save->par_maxes[i] << save->d_short, f);
-  mallocread(save->row_means, ftype, save->d_long, f);
-  mallocread(save->bases, ftype,
-	     save->tries * save->d_long * save->d_short, f);
+#define mallocread(p, t, c, f) \
+  checked_malloc(p, t, c); else \
+  checked_fread(p, sizeof(t), c, f)
+
+#define free_safe(p) if(p != NULL) free(p)
+    
+static char read_save_int(save_t *save, FILE *f) {
+    checked_fread(&save->tries, int, 1, f);
+    checked_fread(&save->n, size_t, 1, f);
+    checked_fread(&save->k, size_t, 1, f);
+    checked_fread(&save->d_short, size_t, 1, f);
+    checked_fread(&save->d_long, size_t, 1, f);
+    mallocread(save->par_maxes, size_t, save->tries, f);
+    mallocread(save->graph, size_t, save->n * save->k, f);
+    checked_malloc(save->which_par, size_t *, save->tries);
+    for(int i = 0; i < save->tries; i++)
+      save->which_par[i] = NULL;
+    for(int i = 0; i < save->tries; i++)
+      mallocread(save->which_par[i], size_t,
+		 save->par_maxes[i] << save->d_short, f);
+    mallocread(save->row_means, ftype, save->d_long, f);
+    mallocread(save->bases, ftype,
+	       save->tries * save->d_long * save->d_short, f);
+    return(0);
+}
+
+char read_save(save_t *save, FILE *f) {
+  save->tries = 0;
+  save->par_maxes = save->graph = NULL;
+  save->which_par = NULL;
+  save->row_means = save->bases = NULL;
+  
+  char r = read_save_int(save, f);
+  if(r) {
+    free_safe(save->bases);
+    free_safe(save->row_means);
+    if(save->which_par != NULL)
+      for(int i = 0; i < save->tries; i++)
+	free_safe(save->which_par[i]);
+    free_safe(save->which_par);
+    free_safe(save->graph);
+    free_safe(save->par_maxes);
+  }
+  return(r);
 }
--- query.c
+++ query.c
@@ -2,40 +2,42 @@
 #include <stdlib.h>
 #include "ann.h"
 
-// Arguments should be in the order:
-// save pts ycnt y use_cpu results dists
-// Last is optional.
-// ycnt and use_cpu are integers, rest are filenames.
+// Arguments:
+// results dists (filenames, second is optional)
+// Reads from stdin (in order):
+// save pts ycnt y use_cpu
+// save is save_t; pts is ftype[]; ycnt is size_t; y is ftype; use_cpu is char.
 int main(int argc, char **argv) {
-  if(argc < 7)
+  if(argc < 2)
     exit(1);
-  FILE *f = fopen(argv[1], "rb");
   save_t save;
-  read_save(&save, f);
-  fclose(f);
+  read_save(&save, stdin);
   size_t k = save.k;
-  f = fopen(argv[2], "rb");
   ftype *pts = malloc(sizeof(ftype) * save.n * save.d_long);
-  fread(pts, sizeof(ftype), save.n * save.d_long, f);
-  fclose(f);
-  size_t ycnt = atol(argv[3]);
+  fread(pts, sizeof(ftype), save.n * save.d_long, stdin);
+  size_t ycnt;
+  fread(&ycnt, sizeof(size_t), 1, stdin);
   ftype *y = malloc(sizeof(ftype) * ycnt * save.d_long);
-  f = fopen(argv[4], "rb");
-  fread(y, sizeof(ftype), ycnt * save.d_long, f);
-  fclose(f);
-  char ucpu = atoi(argv[5]);
+  fread(y, sizeof(ftype), ycnt * save.d_long, stdin);
+  char ucpu;
+  fread(&ucpu, 1, 1, stdin);
   ftype *dists;
   size_t *res;
-  res = query(&save, pts, ycnt, y, argc > 7? &dists : NULL, ucpu);
+  res = query(&save, pts, ycnt, y, argc > 2? &dists : NULL, ucpu);
   free_save(&save);
   free(pts);
   free(y);
-  f = fopen(argv[6], "wb");
-  fwrite(res, sizeof(size_t), ycnt * k, f);
+  FILE *f = fopen(argv[1], "wb");
+  if(f == NULL)
+    return(1);
+  if(fwrite(res, sizeof(size_t), ycnt * k, f) != ycnt * k)
+    return(1);
   fclose(f);
-  if(argc > 7) {
-    f = fopen(argv[7], "wb");
-    fwrite(dists, sizeof(ftype), ycnt * k, f);
+  if(argc > 2) {
+    if((f = fopen(argv[2], "wb")) == NULL)
+      return(1);
+    if(fwrite(dists, sizeof(ftype), ycnt * k, f) != ycnt * k)
+      return(1);
     fclose(f);
   }
 }
